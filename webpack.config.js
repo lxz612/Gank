3b7f4d4abaf74e04156706b7e854b9aa28e77d6c
lxz612 <lxz612@outlook.com>
2016-11-18 23:18:46 +0800

    一点小变动

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,11 @@
 
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 //webpack插件
 var plugins = [
-    // //提公用js到common.js文件中
-    // new webpack.optimize.CommonsChunkPlugin('common.js'),
-    // //将样式统一发布到style.css中
-    // new ExtractTextPlugin("style.css", {
-    //     allChunks: true,
-    //     disable: false
-    // }),
+    //提公用js到common.js文件中
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
     // 使用 ProvidePlugin 加载使用率高的依赖库
     new webpack.ProvidePlugin({
       $: 'webpack-zepto'
@@ -20,27 +14,25 @@ var plugins = [
 ];
 
 module.exports = {
-  entry: './src/main.js',//入口文件
-  output: {//输出配置
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js',
+  entry: './src/main.js', //入口文件
+  output: {               //输出配置
+    path: path.resolve(__dirname, './dist'),//生成文件的存储路径
+    publicPath: '/dist/',                   //？？？
+    filename: 'build.js'                    //生成的文件名
   },
+  //配置loader
   module: {
     loaders: [
-      {
+      {//处理.vue文件
         test: /\.vue$/,
-        loader: 'vue',
-        options: {
-          // vue-loader options go here
-        }
+        loader: 'vue'
       },
-      {
+      {//处理.js文件
         test: /\.js$/,
         loader: 'babel',
         exclude: /node_modules/
       },
-      {
+      {//处理文件中各种图片资源
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file',
         options: {
@@ -57,10 +49,6 @@ module.exports = {
   resolve: {
     // require时省略的扩展名，如：require('module') 不需要module.js
     extension: ['', '.js'],
-    //别名
-    alias: {
-      'vue$': 'vue/dist/vue'
-    }
   },
   plugins:plugins,
   devServer: {
@@ -70,6 +58,7 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
+//生产环境
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
