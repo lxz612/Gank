2bd64b10e764bf998bd3f2e2fbe3267108ee54e1
lxz612 <lxz612@outlook.com>
2016-11-30 18:45:07 +0800

    路由修正

--- src/routers.js
+++ src/routers.js
@@ -4,19 +4,19 @@
 export default function(router) {
 	//路由映射
 	router.map({
-        'Gank/': { //启动页
+        '/': { //启动页
             name: 'start',
             component: function(resolve) {
                 require(['./views/start.vue'], resolve);
             }
         },
-        'Gank/index':{//首页
+        '/index':{//首页
         	name: 'index',
         	component: function(resolve) {
                 require(['./views/index.vue'], resolve);
             }
         },
-        'Gank/meizi':{//纯妹子
+        '/meizi':{//纯妹子
             name:'meizi',
             component:function(resolve){
                 require(['./views/meizi.vue'],resolve);
