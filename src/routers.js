1abb879acfe0d43a3a97b5007f45c4d96c6480f1
lxz612 <lxz612@outlook.com>
2016-11-30 18:20:25 +0800

    路由修正测试

--- src/routers.js
+++ src/routers.js
@@ -4,19 +4,19 @@
 export default function(router) {
 	//路由映射
 	router.map({
-        '/index': { //启动页
+        'Gank/': { //启动页
             name: 'start',
             component: function(resolve) {
                 require(['./views/start.vue'], resolve);
             }
         },
-        '/index2':{//首页
+        'Gank/index':{//首页
         	name: 'index',
         	component: function(resolve) {
                 require(['./views/index.vue'], resolve);
             }
         },
-        '/meizi':{//纯妹子
+        'Gank/meizi':{//纯妹子
             name:'meizi',
             component:function(resolve){
                 require(['./views/meizi.vue'],resolve);
